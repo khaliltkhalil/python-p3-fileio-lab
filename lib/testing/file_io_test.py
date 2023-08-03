1f064ecd367f23c5df015e1a3463c2f1ab2a9fd0
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-08-02 21:04:18 -0500

    write test solutions

--- lib/testing/file_io_test.py
+++ lib/testing/file_io_test.py
@@ -1,13 +1,17 @@
 from file_io import write_file, append_file, read_file
 
+
 def test_write_file(tmp_path):
     """Test write_file()"""
     file_name = tmp_path / "test_file"
     file_content = "This is a test content."
+    print("Hi")
+    print(file_name)
     write_file(file_name, file_content)
-    with open(f'{file_name}.txt', 'r') as f:
+    with open(f"{file_name}.txt", "r") as f:
         file_content_read = f.read()
     assert file_content_read == file_content
+
 
 def test_append_file(tmp_path):
     """Test append_file()"""
@@ -17,9 +21,10 @@ def test_append_file(tmp_path):
     append_content = "\nAppended content."
     write_file(file_name, file_content)
     append_file(file_name, append_content)
-    with open(f'{file_name}.txt', 'r') as f:
+    with open(f"{file_name}.txt", "r") as f:
         file_content_read = f.read()
     assert file_content_read == file_content + append_content
+
 
 def test_read_file(tmp_path):
     """Test read_file()"""
